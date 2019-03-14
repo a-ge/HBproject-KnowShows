80ca41fb74ccbc769814e02f791cc55e5a7d68a5
Ashley <amgesmundo@gmail.com>
2019-03-14 05:44:30 +0000

    Worked on check event layout.

--- server.py
+++ server.py
@@ -275,13 +275,13 @@ def display_event(event_id):
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = False
+    app.debug = True
     # make sure templates, etc. are not cached in debug mode
     app.jinja_env.auto_reload = app.debug
 
     connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
     app.run(port=5000, host='0.0.0.0')
